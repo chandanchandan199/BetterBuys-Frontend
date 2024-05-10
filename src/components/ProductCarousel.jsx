a7460233a226b641db82777c0fd79a353023ae12
Chandan <chandanchandan1996@gmail.com>
2024-05-10 12:28:56 +0530

    Modification of Carousel

--- src/components/ProductCarousel.jsx
+++ src/components/ProductCarousel.jsx
@@ -12,13 +12,14 @@ const ProductCarousel = () => {
   ) : error ? (
     <Message variant="danger">{error}</Message>
   ) : (
-    <Carousel puase="hover" className="bg-primary mb-4">
+    <Carousel pause="hover" className="bg-primary mb-4">
       {products.map((product) => (
         <Carousel.Item key={product._id}>
-          <Link to={`/product/${product._id}`}>
-            <Image src={product.image} />
+          <Link to={`/product/${product._id}`} className="d-block">
+            <Image src={product.image} fluid />{" "}
+            {/* Use fluid for responsive images */}
             <Carousel.Caption className="carousel-caption">
-              <h2>
+              <h2 className="text-center">
                 {product.name} ${product.price}
               </h2>
             </Carousel.Caption>
